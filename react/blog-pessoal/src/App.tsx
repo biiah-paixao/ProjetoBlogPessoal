47ceee7268029fdebdf285e436228393fa83cd6b
Beatriz Paixão <biaperon2001@gmail.com>
2021-10-26 11:22:57 -0300

    Iniciando estudos React

---
+++ react/blog-pessoal/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://i.imgur.com/oGUnytW.jpg" alt="Imagem Tela Inicial" className='img'/>
+        </>
+    );
+}
+
+export default Home;
