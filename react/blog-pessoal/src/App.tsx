79a274d38cdea248e4d52eaac0ee97dcc9f64723
Beatriz Paixão <biaperon2001@gmail.com>
2021-10-26 20:16:25 -0300

    Componentes Navbar e Footer utilizando Material-ui

--- react/blog-pessoal/src/App.tsx
+++ react/blog-pessoal/src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
 import Home from './pages/Home/Home';
 import './App.css';
 
 
 function App() {
   return (
-   <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
+
+/*<Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home />
+        </Grid>
+      </Grid>*/
