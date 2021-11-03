d8d3415afaa0bbd7c547253c3152d5c2e59cb1c7
Beatriz Paixão <biaperon2001@gmail.com>
2021-11-02 21:30:27 -0300

    Mudança do Blog Pessoal

--- react/blog-pessoal/src/pages/home/Home.tsx
+++ react/blog-pessoal/src/pages/home/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import {Typography, Box, Grid, Button} from '@material-ui/core';
+import { Typography, Box, Grid } from '@material-ui/core';
 import './Home.css';
-import { red } from '@material-ui/core/colors';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
 function Home() {
     return (
@@ -20,9 +24,137 @@ function Home() {
                     </Box>
                 </Grid>
                 <Grid item xs={7}>
-                    <img src="https://i.imgur.com/8P1ChWX.png" alt="" className="img"/>
+                    <img src="https://i.imgur.com/8P1ChWX.png" alt="" className="img" />
                 </Grid>
                 <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+
+            <Grid container direction="row" display="flex"  justifyContent="center" alignItems="center">
+                <Grid item xs={2} className="card">
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image="https://avatars.githubusercontent.com/u/47614833?v=4  "
+                                alt=""
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Julia Farias
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={2} className="card">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image="https://avatars.githubusercontent.com/u/88987516?v=4"
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Vitor Santana
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={2} className="card">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image="https://avatars.githubusercontent.com/u/88353298?v=4"
+                                alt=""
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Beatriz Paixão
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={2} className="card">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image="https://avatars.githubusercontent.com/u/89790032?v=4"
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Gustavo S.
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={2} className="card">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image="https://avatars.githubusercontent.com/u/86704425?v=4"
+                                alt="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Jessica Lemos
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                        </CardActions>
+                    </Card>
                 </Grid>
             </Grid>
         </>
